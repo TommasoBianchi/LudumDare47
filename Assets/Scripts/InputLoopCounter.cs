a228f1d435f7d52ababe0f221a20568ff0e00ee5
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2020-10-03 14:49:08 +0200

    Implement adaptive circle center

--- Assets/Scripts/InputLoopCounter.cs
+++ Assets/Scripts/InputLoopCounter.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public class InputLoopCounter : MonoBehaviour
 {
 
     private float degreesCounter;
-    private Vector2 screenCenter;
+    private Vector2 circleCenter;
     private Vector2 previousMousePosition;
     private bool circleStarted;
     private float circleStartTime;
+    private List<Vector2> lastCirclePoints;
 
     public static event Action<float> onCircleCompleted;
 
     private void Start()
     {
-        screenCenter = new Vector2(Screen.width / 2.0f, Screen.height / 2.0f);
+        UpdateCircleCenter(new Vector2(Screen.width / 2.0f, Screen.height / 2.0f), averageWithPrevious: false);
+        lastCirclePoints = new List<Vector2>();
         circleStarted = false;
     }
 
@@ -29,33 +33,25 @@ public class InputLoopCounter : MonoBehaviour
 
         if (Input.GetMouseButton(0))
         {
-            float angle = Vector2.SignedAngle(currentMousePosition - screenCenter, previousMousePosition - screenCenter);
+            float angle = Vector2.SignedAngle(currentMousePosition - circleCenter, previousMousePosition - circleCenter);
+            lastCirclePoints.Add(currentMousePosition);
 
             if (angle < 0)
             {
-                degreesCounter = 0;
-                circleStarted = false;
+                StopCircle();
             }
             else
             {
                 if (!circleStarted)
                 {
-                    circleStarted = true;
-                    circleStartTime = Time.time;
+                    StartCircle();
                 }
 
                 degreesCounter += angle;
 
                 if (degreesCounter >= 360.0f)
                 {
-                    circleStarted = false;
-                    degreesCounter -= 360.0f;
-                    float circleDuration = Time.time - circleStartTime;
-
-                    if (onCircleCompleted != null)
-                    {
-                        onCircleCompleted(circleDuration);
-                    }
+                    FinishCircle();
                 }
             }
 
@@ -64,8 +60,49 @@ public class InputLoopCounter : MonoBehaviour
 
         if (Input.GetMouseButtonUp(0))
         {
-            degreesCounter = 0;
-            circleStarted = false;
+            StopCircle();
         }
+    }
+
+    private void StartCircle()
+    {
+        circleStarted = true;
+        circleStartTime = Time.time;
+    }
+
+    private void FinishCircle()
+    {
+        circleStarted = false;
+        degreesCounter -= 360.0f;
+        float circleDuration = Time.time - circleStartTime;
+
+        if (onCircleCompleted != null)
+        {
+            onCircleCompleted(circleDuration);
+        }
+
+        // Compute new circle center
+        Vector2 lastCirclePointsSum = lastCirclePoints.Aggregate<Vector2, Vector2>(Vector2.zero, (a, b) => a + b);
+        Vector2 lastCirclePointsCenter = lastCirclePointsSum / Mathf.Max(1, lastCirclePoints.Count);
+        UpdateCircleCenter(lastCirclePointsCenter);
+        lastCirclePoints.Clear();
+    }
+
+    private void StopCircle()
+    {
+        degreesCounter = 0;
+        circleStarted = false;
+        lastCirclePoints.Clear();
+    }
+
+    private void UpdateCircleCenter(Vector2 newCircleCenter, bool averageWithPrevious = true)
+    {
+        if (averageWithPrevious)
+        {
+            newCircleCenter = (circleCenter + newCircleCenter) / 2.0f;
+        }
+
+        circleCenter = newCircleCenter;
+        transform.position = Camera.main.ScreenToWorldPoint((Vector3)circleCenter + Vector3.forward * 10);
     }
 }
