de52f70e676607d005ea855068735865f9ec65b5
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2020-10-04 17:42:32 +0200

    Add dynamic color and alpha to wheel trail

---
+++ Assets/Scripts/WheelTrail.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class WheelTrail : MonoBehaviour
+{
+
+    [SerializeField]
+    private Color slowColor;
+    [SerializeField]
+    private Color fastColor;
+    [SerializeField]
+    private float fastSpeedTreshold;
+    [SerializeField, Range(0.0f, 1.0f)]
+    private float alphaMultiplier;
+
+    private TrailRenderer trailRenderer;
+
+    private void Start()
+    {
+        trailRenderer = GetComponent<TrailRenderer>();
+
+        WheelMover.onWheelVelocityUpdate += (speed) =>
+        {
+            float fastness = Mathf.Clamp01(speed / fastSpeedTreshold);
+
+            Gradient gradient = trailRenderer.colorGradient;
+
+            GradientAlphaKey[] alphaKeys = gradient.alphaKeys;
+            GradientColorKey[] colorKeys = gradient.colorKeys;
+
+            Debug.Log("Alpha key 0 has alpha = " + alphaKeys[0].alpha + " and time = " + alphaKeys[0].time);
+
+            alphaKeys[0].alpha = fastness * alphaMultiplier;
+            colorKeys[0].color = Color.Lerp(slowColor, fastColor, fastness);
+
+            gradient.SetKeys(colorKeys, alphaKeys);
+
+            trailRenderer.colorGradient = gradient;
+        };
+    }
+}
